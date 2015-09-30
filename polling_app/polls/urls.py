1daf8922d1ecba02916b16fd19eda6a600471982
Calvin Studebaker <calstud@stanford.edu>
2015-09-30 11:40:43 -0700

    index view tests

--- polling_app/polls/urls.py
+++ polling_app/polls/urls.py
@@ -4,15 +4,15 @@ from django.conf.urls import url
 urlpatterns = [
     #index page
     #ex: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     #detail page
     #ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     #results page
     #ex: /polls/5/results
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 
     #voting page
     #ex: /polls/5/vote/
